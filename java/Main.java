765fa4f56ab9e8e8638f1f0f11327b77e8aec579
Tamerlan2006 <241648@astanait.edu.kz>
2025-09-27 21:44:09 +0500

    golang library works

--- java/Main.java
+++ java/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Validate validator = new GoValidateAdapter();
 
-        String test1 = "79927398713";   // корректный по алгоритму Луна
-        String test2 = "1234567890";    // некорректный
+        String test1 = "79927398713";  
+        String test2 = "1234567890";    
 
         System.out.println("Test1: " + test1 + " → " + validator.validate(test1));
         System.out.println("Test2: " + test2 + " → " + validator.validate(test2));
